e963242107ffbff9fa59a2d6e81748e019b235fb
foysal41 <mdfoysal.mf33@gmail.com>
2024-12-08 02:38:27 +0600

    show index page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <title>Student Laravel 9 CRUD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
 <body>
@@ -44,31 +44,12 @@
                 </div>
                 <div class="col-md-9">
                     <!-- Page content -->
-                    <div class="content">
-                         @yield('content')
-                    </div>
+                    @yield('content')
                 </div>
             </div>
         </div>
     </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="container">
-        @yield('content')
-    </div>
 
 </body>
 
